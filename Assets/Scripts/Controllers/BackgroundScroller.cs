cebc233d337e02ab9ff92a5cb19833d0a24428e3
RVerhoef <r.verhoef@hotmail.nl>
2015-11-10 13:40:10 +0100

    Added scrolling background & interactable spawner

---
+++ Assets/Scripts/Controllers/BackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿//Written by Rob Verhoef
+using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    private float _speed = 0.005f;
+    private float _offset;
+    private Renderer _renderer;
+
+	void Awake ()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+	
+	void Update ()
+    {
+        _offset += _speed;
+        if (_offset > 1.0f)
+        {
+            _offset -= 1.0f;
+            Debug.Log("Scrolling");
+        }
+        _renderer.material.mainTextureOffset = new Vector2 (_offset, 0);
+	}
+}
---
+++ Assets/Scripts/Controllers/InteractableSpawner.cs
@@ -0,0 +1,40 @@
+﻿//Written by Rob Verhoef
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random; 
+
+public class InteractableSpawner : MonoBehaviour 
+{
+    [SerializeField] private Object[] _interactables;
+    private Object _chosenInteractable;
+    private float _randomSpawntime;
+    private bool _spawning;
+
+    void Awake ()
+    {
+        _interactables = Resources.LoadAll("Prefabs/Interactable/Formations", typeof (Object));
+    }
+
+    void Update()
+    {
+        //Spawns an interactable after a randomly determined amount of time, or reduces the spawning timer
+        if (_spawning == false && _randomSpawntime == 0)
+        {
+            Invoke("Spawn", 25);
+            _spawning = true;
+        }
+        else if (_randomSpawntime > 0)
+        {
+            _randomSpawntime--;
+        }
+    }
+
+    void Spawn ()
+    {
+        //A random interactable is chosen & spawned
+        _chosenInteractable = _interactables[Random.Range(0, _interactables.Length)];
+        Instantiate(_chosenInteractable, new Vector2(Random.Range(-11, 11), transform.position.y), transform.rotation);
+        _randomSpawntime = Random.Range(15, 101);
+        _spawning = false;
+    }
+}

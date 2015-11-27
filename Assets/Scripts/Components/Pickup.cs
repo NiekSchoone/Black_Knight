fe638458884fc97ac4e92415707602b9c36cde9d
NiekSchoone <16789@ma-web.nl>
2015-11-27 09:21:03 +0100

    Addind points

--- Assets/Scripts/Components/Pickup.cs
+++ Assets/Scripts/Components/Pickup.cs
@@ -5,11 +5,18 @@ public class Pickup : MonoBehaviour
 {
     public int itemValue;
     public string picker = "PickupCollider";
+    public GameObject pointManager;
+
+    void Awake()
+    {
+        pointManager = GameObject.Find("PointManager");
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == picker)
         {
+            pointManager.GetComponent<PointManager>().Points += itemValue;
             Destroy(this.gameObject);
         }
     }

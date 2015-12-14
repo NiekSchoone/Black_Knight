1409f0ca14d55b368b6f5c5e8eba24f0a73f2c87
NiekSchoone <16789@ma-web.nl>
2015-12-14 10:04:10 +0100

    Enemy Hit event

--- Assets/Scripts/Components/MeleeAttack.cs
+++ Assets/Scripts/Components/MeleeAttack.cs
@@ -6,7 +6,8 @@ public class MeleeAttack : MonoBehaviour
 {
     private Collider2D attackTrigger;
 
-    public string thingToHit;
+    public string thingToHit1;
+    public string thingToHit2;
 
 	void Start ()
     {
@@ -26,7 +27,7 @@ public class MeleeAttack : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == thingToHit)
+        if (col.gameObject.tag == thingToHit1 || col.gameObject.tag == thingToHit2)
         {
             col.gameObject.GetComponent<HitEvent>().Hit();
         }
